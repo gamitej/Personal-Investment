4220195fde8d9f95b78925eb7928d1a5f8024125
gamitej <gamitejpratapsingh998@gmail.com>
2024-03-16 22:38:45 +0530

    NEW : loader added

--- client/src/components/loader/FullScreenLoader/FullScreenLoader.tsx
+++ client/src/components/loader/FullScreenLoader/FullScreenLoader.tsx
@@ -1,9 +1,32 @@
 import "./FullScreenLoader.scss";
+import { RotatingLines } from "react-loader-spinner";
 
-const FullScreenLoader = () => {
+interface FullScreenLoaderProps {
+  color?: string;
+  width?: string;
+  loadingText?: string;
+}
+
+const FullScreenLoader = ({
+  loadingText = "",
+  width = "60",
+  color = "#5175d9",
+}: FullScreenLoaderProps) => {
   return (
-    <div>
-      <div>loading...</div>
+    <div className="fullscreen-loader">
+      <div className="container">
+        <div>
+          <RotatingLines
+            width={width}
+            visible={true}
+            strokeWidth="5"
+            strokeColor={color}
+            animationDuration="0.75"
+            ariaLabel="rotating-lines-loading"
+          />
+        </div>
+        <div>{loadingText}</div>
+      </div>
     </div>
   );
 };
