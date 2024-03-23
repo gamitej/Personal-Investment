51432d9797e31db9b4c85ed2277b84828bb9523f
gamitej <gamitejpratapsingh998@gmail.com>
2024-03-23 12:20:22 +0530

    FIX : nivo pe chart build

--- client/src/components/charts/DonutChart.tsx
+++ client/src/components/charts/DonutChart.tsx
@@ -1,34 +1,91 @@
-import { ApexOptions } from "apexcharts";
-import ReactApexChart from "react-apexcharts";
+import { FC } from "react";
+import { ResponsivePie } from "@nivo/pie";
 
-const DonutChart = ({
+interface DonutChartProps {
+  series: any;
+  fillData?: any;
+  padAngle?: number;
+  innerRadius?: number;
+  borderWidth?: number;
+  cornerRadius?: number;
+}
+
+const DonutChart: FC<DonutChartProps> = ({
   series = [],
-  chartBreakPoint = 480,
-  legendPosition = "bottom",
-}) => {
-  const options: ApexOptions = {
-    chart: {
-      type: "donut",
-    },
-    responsive: [
+  fillData = [],
+  padAngle = 2,
+  borderWidth = 1,
+  cornerRadius = 10,
+  innerRadius = 0.75,
+}) => (
+  <ResponsivePie
+    data={series}
+    padAngle={padAngle}
+    borderWidth={borderWidth}
+    innerRadius={innerRadius}
+    cornerRadius={cornerRadius}
+    activeOuterRadiusOffset={8}
+    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+    borderColor={{
+      from: "color",
+      modifiers: [["darker", 0.2]],
+    }}
+    arcLabelsSkipAngle={10}
+    arcLinkLabelsThickness={2}
+    arcLinkLabelsSkipAngle={10}
+    arcLinkLabelsTextColor="#333333"
+    arcLinkLabelsColor={{ from: "color" }}
+    arcLabelsTextColor={{
+      from: "color",
+      modifiers: [["darker", 2]],
+    }}
+    defs={[
       {
-        breakpoint: chartBreakPoint,
-        options: {
-          chart: {
-            width: 200,
-          },
-          legend: {
-            position: legendPosition,
-          },
-        },
+        id: "dots",
+        type: "patternDots",
+        background: "inherit",
+        color: "rgba(255, 255, 255, 0.3)",
+        size: 4,
+        padding: 1,
+        stagger: true,
       },
-    ],
-  };
-
-  /**
-   * TSX
-   */
-  return <ReactApexChart type="donut" series={series} options={options} />;
-};
+      {
+        id: "lines",
+        type: "patternLines",
+        background: "inherit",
+        color: "rgba(255, 255, 255, 0.3)",
+        rotation: -45,
+        lineWidth: 6,
+        spacing: 10,
+      },
+    ]}
+    fill={fillData}
+    legends={[
+      {
+        anchor: "bottom",
+        direction: "row",
+        justify: false,
+        translateX: 0,
+        translateY: 56,
+        itemsSpacing: 0,
+        itemWidth: 100,
+        itemHeight: 18,
+        itemTextColor: "#999",
+        itemDirection: "left-to-right",
+        itemOpacity: 1,
+        symbolSize: 18,
+        symbolShape: "circle",
+        effects: [
+          {
+            on: "hover",
+            style: {
+              itemTextColor: "#000",
+            },
+          },
+        ],
+      },
+    ]}
+  />
+);
 
 export default DonutChart;
--- client/src/pages/Expenses/MajorExpense.tsx
+++ client/src/pages/Expenses/MajorExpense.tsx
@@ -1,5 +1,79 @@
+import DonutChart from "@/components/charts/DonutChart";
+
+const fillData = [
+  {
+    match: {
+      id: "ruby",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "c",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "go",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "python",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "scala",
+    },
+    id: "lines",
+  },
+  {
+    match: {
+      id: "lisp",
+    },
+    id: "lines",
+  },
+  {
+    match: {
+      id: "elixir",
+    },
+    id: "lines",
+  },
+  {
+    match: {
+      id: "javascript",
+    },
+    id: "lines",
+  },
+];
+
+const data = [
+  {
+    id: "go",
+    label: "go",
+    value: 39,
+    color: "hsl(243, 70%, 50%)",
+  },
+  {
+    id: "make",
+    label: "make",
+    value: 203,
+    color: "hsl(267, 70%, 50%)",
+  },
+  {
+    id: "erlang",
+    label: "erlang",
+    value: 291,
+    color: "hsl(188, 70%, 50%)",
+  },
+];
+
 const MajorExpense = () => {
-  return <div>MajorExpense</div>;
+  return <DonutChart series={data} />;
 };
 
 export default MajorExpense;
